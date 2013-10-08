33c5ce41c98527a27a64eb850cd18fc56dfc7f96
tshort <tshort.rlists@gmail.com>
2013-10-08 10:06:20 -0400

    Add a test using blend for `@devec_transform`.

--- test/test_extensions.jl
+++ test/test_extensions.jl
@@ -18,7 +18,8 @@ d = {:x => rand(n),
 @test isequal(d[:xd], d[:x] .* d[:y] + d[:z])
 
 @devec_transform(d,
-                 xd => x .* y + z,
-                 yd => x .* x)
+                 xd => x .* y + z, 
+                 yd => x .* x,
+                 zd => blend(y .> 7, z, -z))
 
 @test isequal(d[:yd], d[:x] .* d[:x])
